3f14252f8d04a79741273c5d70bdc259b085bfd7
mike-axiom-mir <miketobi90@gmail.com>
2026-09-09 18:58:09 +0200

    Correct Universal Creation Beacon CLI identity

--- .axm/beacon/beacon.py
+++ .axm/beacon/beacon.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""AXM Organ Beacon v0.1 command line for Game Asset Forge."""
+"""AXM Organ Beacon v0.1 command line for Universal Creation."""
 from __future__ import annotations
 
 import argparse
