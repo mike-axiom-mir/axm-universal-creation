941ece148ec1410caf28091e0373d8b22b0507d8
mike-axiom-mir <miketobi90@gmail.com>
2026-09-15 19:20:45 +0200

    Bind animation adversarial contract proof

--- tests/test_creative_precision_fabric.py
+++ tests/test_creative_precision_fabric.py
@@ -60,6 +60,9 @@ class CreativePrecisionFabricTests(unittest.TestCase):
     def test_rigging_animation_wave(self):
         self.run_node('creative-animation-hands-selftest.js')
 
+    def test_rigging_animation_fail_closed_contracts(self):
+        self.run_node('creative-animation-contract-selftest.js')
+
 
 if __name__ == '__main__':
     unittest.main()
