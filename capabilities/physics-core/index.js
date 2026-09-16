e650d591974c50e1751f4343b7b8ee746e853d72
mike-axiom-mir <miketobi90@gmail.com>
2026-09-16 06:25:32 +0200

    physics: expose constraint composer

--- capabilities/physics-core/index.js
+++ capabilities/physics-core/index.js
@@ -5,5 +5,6 @@ module.exports = {
   sourceAdapter: require('./source/axm-physics-adapter.js'),
   fabric: require('./uc-physics-fabric.js'),
   distanceJoints: require('./uc-distance-joints.js'),
-  translationMounts: require('./uc-translation-mounts.js')
+  translationMounts: require('./uc-translation-mounts.js'),
+  constraintComposer: require('./uc-constraint-composer.js')
 };
