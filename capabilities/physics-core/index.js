a335848a0d9a483a70030f76196824d5a8e731df
mike-axiom-mir <miketobi90@gmail.com>
2026-09-16 04:13:18 +0200

    physics: expose core adapter and UC fabric capsule

---
+++ capabilities/physics-core/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  core: require('./source/axm-physics-core.js'),
+  sourceAdapter: require('./source/axm-physics-adapter.js'),
+  fabric: require('./uc-physics-fabric.js')
+};
