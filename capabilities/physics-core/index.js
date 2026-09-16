6edcbe855002e05d816313b45b56e8be7b646f6b
mike-axiom-mir <miketobi90@gmail.com>
2026-09-16 14:27:27 +0200

    chore: export axis limits capability

--- capabilities/physics-core/index.js
+++ capabilities/physics-core/index.js
@@ -8,6 +8,7 @@ module.exports = {
   distanceLimits: require('./uc-distance-limits.js'),
   translationMounts: require('./uc-translation-mounts.js'),
   axisLocks: require('./uc-axis-locks.js'),
+  axisLimits: require('./uc-axis-limits.js'),
   constraintComposer: require('./uc-constraint-composer.js'),
   constraintCollisionIsolation: require('./uc-constraint-collision-isolation.js'),
   constraintActivityGate: require('./uc-constraint-activity-gate.js')
